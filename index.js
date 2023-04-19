d6f3063e7faadf7d7ff278b74f1c89049a39ddab
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-04-19 19:48:53 -0300

    criado uma nova rota, [M1S06] Ex 4 - Rotas com express.

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/', (req, res) => {
   res.send('Sendo perseverante chegarei até o final do curso, lutarei para sair com o certificado ');
 });
 
+app.get('/api/:nome', (req, res) => {
+    const nome = req.params.nome;
+    console.log(`Rota de API criada pelo(a): ${nome}`);
+    res.send(`Olá, ${nome}!`);
+  });
+
 app.listen(1987, () => {
   console.log('Servidor iniciado na porta 1987!');
 });
