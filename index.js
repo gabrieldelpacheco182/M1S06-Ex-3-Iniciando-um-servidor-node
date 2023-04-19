75b67d39f7c5669e5a9103f08d02b971d73f6886
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-04-19 19:58:29 -0300

    rota saudação [M1S06] Ex 5 - Rotas com express.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Sendo perseverante chegarei até o final do curso, lutarei para sair com o certificado ');
+app.get('/', (request, response) => {
+  request.send('Sendo perseverante chegarei até o final do curso, lutarei para sair com o certificado ');
 });
 
-app.get('/api/:nome', (req, res) => {
-    const nome = req.params.nome;
+app.get('/api/:nome', (request, response) => {
+    const nome = request.params.nome;
     console.log(`Rota de API criada pelo(a): ${nome}`);
     res.send(`Olá, ${nome}!`);
   });
@@ -14,3 +14,12 @@ app.get('/api/:nome', (req, res) => {
 app.listen(1987, () => {
   console.log('Servidor iniciado na porta 1987!');
 });
+
+app.use(express.json());
+
+app.post('/api/objeto', (request, response) => {
+  const objeto = request.body;
+  console.log('Objeto recebido:', objeto);
+  response.json(objeto);
+});
+
